26863039f9949c9ffa72cffd1f97de7abd138e92
bound1ess <bound1ess.dev@gmail.com>
2016-02-13 19:19:41 +0600

    Rewrite rational helper.

--- src/helpers/rational.php
+++ src/helpers/rational.php
@@ -4,12 +4,6 @@ use Math\{Rational, Integer};
 
 if ( ! function_exists('rational')) {
     function rational(int $num = null, int $denom = null): Rational {
-        if (null == $num) {
-            $num = 0;
-        }
-        if (null == $denom) {
-            $denom = 1;
-        }
-        return new Rational(new Integer($num), new Integer($denom));
+        return new Rational(new Integer($num ?? 0), new Integer($denom ?? 1));
     }
 }
